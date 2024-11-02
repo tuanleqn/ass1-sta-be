18eab246304b2ecc4e42ada8bff46c1a6a11ba0a
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2024-11-02 22:14:12 +0700

    update

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from factory import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    port = int(os.environ.get("PORT", 10000))  # Lấy cổng từ biến môi trường, mặc định là 10000 nếu không có
-    app.run(host="0.0.0.0", port=port)
+    app.run()
